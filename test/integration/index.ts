06202d1905edd2fa3164caf47ea5ac60611b7f1f
Vitaly Turovsky <vital2580@icloud.com>
2022-02-09 08:10:34 +0300

    enable basic integration tests

---
+++ test/integration/index.ts
@@ -0,0 +1,35 @@
+import { join } from 'path'
+import glob from 'glob'
+import Mocha from 'mocha'
+import * as jest from 'jest'
+
+export const run = async () => {
+    // can't require vscode as no way to disable sandboxing https://github.com/facebook/jest/issues/8010
+    // await jest.runCLI(
+    //     {
+    //         ci: true,
+    //         colors: true,
+    //         runInBand: true,
+    //         $0: '',
+    //         _: [],
+    //         setupFilesAfterEnv: [join(__dirname, 'setup.js')],
+    //     },
+    //     [join(__dirname, '.')],
+    // )
+    const mocha = new Mocha({
+        color: true,
+    })
+    const testsRoot = join(__dirname, './suite')
+    await new Promise<void>(resolve => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) throw err
+
+            for (const file of files) mocha.addFile(join(testsRoot, file))
+
+            mocha.run(failures => {
+                if (failures > 0) throw new Error(`${failures} tests failed.`)
+                else resolve()
+            })
+        })
+    })
+}
--- test/integration/runTests.ts
+++ test/integration/runTests.ts
@@ -1,16 +1,16 @@
-import { runTests } from '@vscode/test-electron'
 import { join } from 'path'
+import { runTests } from '@vscode/test-electron'
 
 async function main() {
     try {
         await runTests({
-            // version: 'insiders',
+            version: '1.64.0',
             extensionDevelopmentPath: join(__dirname, '../out'),
             extensionTestsPath: join(__dirname, './index'),
             launchArgs: ['--disable-extensions'],
         })
-    } catch (err) {
-        console.error(err)
+    } catch (error) {
+        console.error(error)
         console.error('Failed to run tests')
         process.exit(1)
     }
