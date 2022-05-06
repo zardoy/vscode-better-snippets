a4bb2aaa0474e8cef716143030950e37bd053272
Vitaly Turovsky <vital2580@icloud.com>
2022-05-06 06:00:32 +0300

    refactor: use vscode-utils patches

--- vscode-framework.config.js
+++ vscode-framework.config.js
@@ -1,21 +1,31 @@
 //@ts-check
+const { defineConfig } = require('@zardoy/vscode-utils/build/defineConfig.cjs')
+const { patchPackageJson } = require('@zardoy/vscode-utils/build/patchPackageJson.cjs')
 
-/** @type{import('vscode-framework/build/config').UserConfig} */
-const config = {
-    esbuild: {
-        production: {
-            defineEnv: {
-                EXTENSION_BOOTSTRAP_CONFIG: null,
-            },
-        },
-        mainFields: ['module', 'main'],
+patchPackageJson({
+    async patchSettings(configuration) {
+        configuration['customSnippetDefaults']['properties']['type']['deprecationMessage'] = configuration['customSnippets']['items']['allOf'][1]['properties'][
+            'type'
+        ]['deprecationMessage'] = 'Use `iconType` instead. It will be removed in next minor release'
+
+        configuration['customSnippets']['items']['allOf'][1]['properties']['group']['deprecationMessage'] = configuration['customSnippetDefaults'][
+            'properties'
+        ]['group']['deprecationMessage'] = 'Use `description` instead. It will be removed in next minor release'
+
+        const { defaultLanguageSupersets } = await import('@zardoy/vscode-utils/build/langs.js')
+        // TODO move it from here
+        configuration['languageSupersets'].default = defaultLanguageSupersets
+        return configuration
     },
+})
+
+const config = defineConfig({
     development: {
         // disableExtensions: false,
     },
     // target: 'web',
     // webOpen: 'web',
     target: { desktop: true, web: true },
-}
+})
 
 module.exports = config
